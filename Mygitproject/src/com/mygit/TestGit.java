297f9881bafc6a07ac01c01f04a0a431d5e2aae8
iternia <iternia@PC-5>
2017-01-19 10:47:08 +0530

    my third change

--- Mygitproject/src/com/mygit/TestGit.java
+++ Mygitproject/src/com/mygit/TestGit.java
@@ -5,7 +5,7 @@ public class TestGit {
 	public static void main(String[] args) {
 		System.out.println("My use of github");
 		System.out.println("My second change");
-
+        System.out.println("My third change");
 	}
 
 }

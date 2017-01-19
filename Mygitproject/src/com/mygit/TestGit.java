6607d5148c142273821b717a79c9b73732acfc5d
iternia <iternia@PC-5>
2017-01-19 10:48:21 +0530

    comment to second system.out

--- Mygitproject/src/com/mygit/TestGit.java
+++ Mygitproject/src/com/mygit/TestGit.java
@@ -4,7 +4,7 @@ public class TestGit {
 
 	public static void main(String[] args) {
 		System.out.println("My use of github");
-		System.out.println("My second change");
+		//System.out.println("My second change");
         System.out.println("My third change");
 	}
 

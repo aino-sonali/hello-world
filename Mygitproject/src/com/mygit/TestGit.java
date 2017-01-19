237ce67ebd11ae28bc7ed7355ee7e808d5d5ded5
aino-ashwini <ashwini@ainosoft.com>
2017-01-19 11:15:34 +0530

    statement added

--- Mygitproject/src/com/mygit/TestGit.java
+++ Mygitproject/src/com/mygit/TestGit.java
@@ -5,7 +5,8 @@ public class TestGit {
 	public static void main(String[] args) {
 		System.out.println("My use of github");
 		//System.out.println("My second change");
-        System.out.println("My third change");
+        	System.out.println("My third change");
+		System.out.println("Statement added by ashwini");
 	}
 
 }
